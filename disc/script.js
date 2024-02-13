708cc5dcab4fe17026dab88ac56cfc1114cbaa27
Jayanta Ojha <jayantaojha686@gmail.com>
2024-02-14 00:03:03 +0530

    All links are added

---
+++ disc/script.js
@@ -0,0 +1,75 @@
+const body = document.querySelector('body');
+const gmail = document.querySelector('#gmail');
+const twitter = document.querySelector('#twitter');
+const github = document.querySelector('#github');
+const skype = document.querySelector('#skype');
+const linkedin = document.querySelector('#linkedin');
+const youtube1 = document.querySelector('#youtube1');
+const youtube2 = document.querySelector('#youtube2');
+const whatsapp = document.querySelector('#whatsapp');
+const medium = document.querySelector('#medium');
+const maps = document.querySelector('#maps');
+const discord = document.querySelector('#discord');
+const telegram = document.querySelector('#telegram');
+
+
+
+gmail.addEventListener('click', () => {
+    alert('Gmail ID:   jayantaojha686@gmail.com');
+})
+
+
+twitter.addEventListener('click', () => {
+    window.open('https://twitter.com/jayantaojha172', '_blank');
+})
+
+
+github.addEventListener('click', () => {
+    window.open('https://github.com/Jayantaojha', '_blank');
+})
+
+
+skype.addEventListener('click', () => {
+    window.open('https://join.skype.com/invite/qGVUDjzz6drF', '_blank');
+})
+
+
+linkedin.addEventListener('click', () => {
+    window.open('https://www.linkedin.com/in/jayanta-ojha-21096520b/', '_blank');
+})
+
+
+youtube1.addEventListener('click', () => {
+    window.open('https://www.youtube.com/@dev.daily_code/featured', '_blank');
+})
+
+
+youtube2.addEventListener('click', () => {
+    window.open('https://www.youtube.com/@nscb-sv', '_blank');
+})
+
+
+whatsapp.addEventListener('click', () => {
+    window.open('https://whatsapp.com/channel/0029VaJISNXJpe8juUnWhF1J', '_black');
+})
+
+
+medium.addEventListener('click', () => {
+    window.open('https://medium.com/@jayanta.ojha22', '_black');
+})
+
+
+maps.addEventListener('click', () => {
+    window.open('https://www.google.com/maps/contrib/108511408936138045230/contribute/@26.7335908,88.3540897,13z/data=!3m1!4b1!4m3!8m2!3m1!1e1?entry=ttu', '_blank');
+})
+
+
+discord.addEventListener('click', () => {
+    alert('User Name:  jayantaojha_72074');
+})
+
+
+telegram.addEventListener('click', () => {
+    window.open('https://t.me/jayanta_ojha', '_blank');
+})
+
